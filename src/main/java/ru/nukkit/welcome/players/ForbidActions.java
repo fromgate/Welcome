567f48fced439380030d36930d7cf22f00032fe7
Pub4Game <Pub4Game@users.noreply.github.com>
2016-03-15 16:46:47 +0300

    Improvement events

--- src/main/java/ru/nukkit/welcome/players/ForbidActions.java
+++ src/main/java/ru/nukkit/welcome/players/ForbidActions.java
@@ -8,11 +8,10 @@ import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.block.BlockBreakEvent;
 import cn.nukkit.event.block.BlockPlaceEvent;
-import cn.nukkit.event.block.SignChangeEvent;
 import cn.nukkit.event.entity.EntityDamageByChildEntityEvent;
 import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.event.entity.EntityDamageEvent;
-import cn.nukkit.event.inventory.*;
+import cn.nukkit.event.inventory.CraftItemEvent;
 import cn.nukkit.event.player.*;
 import ru.nukkit.welcome.commands.Commander;
 import ru.nukkit.welcome.util.Message;
@@ -40,37 +39,7 @@ public class ForbidActions implements Listener {
             Entity damager = ((EntityDamageByChildEntityEvent) event).getDamager();
             if (damager instanceof Player) player = (Player) damager;
         }
-        if (player!=null) cancel(player, event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onSignChange(SignChangeEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onInventoryItemPickup(InventoryPickupItemEvent event) {
-        for (Player player : event.getViewers()) {
-            cancel(player, event);
-        }
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onInventoryArrowPickup(InventoryPickupArrowEvent event) {
-        for (Player player : event.getViewers()) {
-            cancel(player, event);
-        }
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-
-    public void onInventoryOpenEvent(InventoryOpenEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onInventoryCloseEvent(InventoryCloseEvent event) {
-        cancel(event.getPlayer(), event);
+        if (player != null) cancel(player, event);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
@@ -84,43 +53,19 @@ public class ForbidActions implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerAnimationEvent(PlayerAnimationEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerBedEnterEvent(PlayerBedEnterEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerBedLeaveEvent(PlayerBedLeaveEvent event) {
-        if (!PlayerManager.isPlayerLoggedIn(event.getPlayer())) event.setCancelled();
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerBucketEmptyEvent(PlayerBucketEmptyEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerBucketFillEvent(PlayerBucketFillEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerChatEvent(PlayerChatEvent event) {
         cancel(event.getPlayer(), event);
-        if (event.isCancelled()) Message.debugMessage("PlayerChatEvent is cancelled:",event.getMessage());
+        if (event.isCancelled()) Message.debugMessage("PlayerChatEvent is cancelled:", event.getMessage());
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerCommandPreprocessEvent(PlayerCommandPreprocessEvent event) {
-        Message.debugMessage("PlayerManager.isPlayerLoggedIn(event.getPlayer()) : "+PlayerManager.isPlayerLoggedIn(event.getPlayer()));
+        Message.debugMessage("PlayerManager.isPlayerLoggedIn(event.getPlayer()) : " + PlayerManager.isPlayerLoggedIn(event.getPlayer()));
         if (PlayerManager.isPlayerLoggedIn(event.getPlayer())) return;
         String cmd = Commander.getCommandByAlias(event.getMessage().substring(1).split(" ")[0]);
-        Message.debugMessage("cmd: "+(cmd==null ? "null" : cmd));
-        if (cmd!=null&&(cmd.equalsIgnoreCase("register")||cmd.equalsIgnoreCase("login"))) return; // Разрешаем только команды для регистрации
+        Message.debugMessage("cmd: " + (cmd == null ? "null" : cmd));
+        if (cmd != null && (cmd.equalsIgnoreCase("register") || cmd.equalsIgnoreCase("login")))
+            return; // Разрешаем только команды для регистрации
         event.setCancelled();
     }
 
@@ -135,17 +80,7 @@ public class ForbidActions implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerGameModeChangeEvent(PlayerGameModeChangeEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerInteractEvent(PlayerInteractEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerInvalidMoveEvent(PlayerInvalidMoveEvent event) {
         cancel(event.getPlayer(), event);
     }
 
@@ -155,22 +90,7 @@ public class ForbidActions implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerItemHeldEvent(PlayerItemHeldEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerMoveEvent(PlayerMoveEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerToggleSneakEvent(PlayerToggleSneakEvent event) {
-        cancel(event.getPlayer(), event);
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onPlayerToggleSprintEvent(PlayerToggleSprintEvent event) {
         cancel(event.getPlayer(), event);
     }
 
